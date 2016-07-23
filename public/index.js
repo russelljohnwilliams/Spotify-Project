d1a8edbdcbddb2238db46d23993306d30b8a2f09
Russell Williams <russellwilliamscc@gmail.com>
2016-07-23 16:27:24 +0100

    adds select song

--- public/index.js
+++ public/index.js
@@ -1,13 +1,9 @@
-// var state = {
-//   spotifySearch: null
-// } 
-
-// console.log("outter", state.spotifySearch)
-
-// var song
-
 var play = {
   song: null
+}
+
+var state = {
+  singles: null
 }
 
 window.onload = function(){
@@ -21,21 +17,19 @@ var main = function() {
   var form = document.getElementById( 'search' );
   form.onsubmit = handleSubmit;
 
-  // var playButton = document.getElementById( 'play' );
-
   var btn = document.getElementById('playMusic');
   btn.onclick = playAudio;
 
+  // var select = document.getElementById('songList');
 
 };
 
 var handleClick = function() {
   var searchedItem = document.getElementById( 'search-input' )
   var userInput = searchedItem.value;
-  searchedItem.value = '';
+  // searchedItem.value = '';
   searchSpotify(userInput)
 };
-
 
 var handleSubmit = function( event ){
   event.preventDefault();
@@ -43,7 +37,6 @@ var handleSubmit = function( event ){
 };
 
 var searchSpotify = function(userInput) {
-
   var url = "https://api.spotify.com/v1/search?q=" + userInput + '&type=artist';
   var request = new XMLHttpRequest();
   request.open("GET", url);
@@ -52,62 +45,82 @@ var searchSpotify = function(userInput) {
       var jsonString = request.responseText;
       spotifySearch = JSON.parse( jsonString );
       searchAlbums(spotifySearch)
-      console.log('spotifySearch', spotifySearch)
+      // console.log('spotifySearch', spotifySearch)
     };
   };
   request.send( null );
-
 };
 
 var searchAlbums = function(spotifySearch){
-
+  // console.log("searchAlbums func", spotifySearch)
   var artist = spotifySearch.artists.items[0].href;
-
   var url = artist + "/albums?album_type=single";
   var request1 = new XMLHttpRequest();
   request1.open("GET", url);
   request1.onload = function(){
     if(request1.status === 200) {
-      var singles = JSON.parse(request1.responseText)
-      console.log(singles)
-      var x = singles.items[0].href
-      console.log("that time?", x)
-      searchForSong(x)
+      state.singles = JSON.parse(request1.responseText)
+      // console.log("sA, singles", singles)
+      dropDownList()
+      // var singles = singles.items[1].href
+      // console.log("that time?", x)
+      // searchForSong(singles)
     }
   };
   request1.send(null)
 };
 
+var dropDownList = function(){
+  var select = document.getElementById('songList');
+  // console.log("ddlist singles", singles)
+  for ( var i = 0; i < state.singles.items.length; i++ ){
+    var opt = document.createElement('option')
+    opt.innerHTML = i;
+    opt.value = i;
+    // console.log("opt Value", opt)
+    select.appendChild(opt)
+  }
+}
 
-var searchForSong = function(songSearch){
+var findSong = function(songSearch){
+  // console.log("search for a star", songSearch)
   var url = songSearch;
   var request1 = new XMLHttpRequest();
   request1.open("GET", url);
   request1.onload = function(){
     if(request1.status === 200) {
       var singles = JSON.parse(request1.responseText)
-      console.log(singles)
+      console.log("whaaaaat", singles.tracks.items[0].preview_url)
       play.song = singles.tracks.items[0].preview_url
-      // console.log("inner song?", song)
-      // var a = new Audio(song);
+      // var a = new Audio(play.song);
       // a.play();
-      // console.log('a', a)
+      // console.log('play?', play.song)
     }
   };
   request1.send(null)
 };
 
-// console.log('outer', song) 
+  // var single = singles.items[i].href
 
-// playButton.onclick = function(song){
-//   console.log("onclick song", song)
-//       var a = new Audio(song);
-//       a.play();
+
+// var displayOptions = function(select) {
+//   for ( var i = 0; i < state.countries.length; i++ ) {
+//     var opt = document.createElement('option')
+//     opt.innerHTML = state.countries[i].name;
+//     opt.value = i;
+//     select.appendChild(opt)
+//   }
 // }
 
+var selectSong = function(){
+  var index = document.getElementById('songList').value;
+  var song = state.singles.items[index].href
+  console.log("what's this", song)
+  findSong(song)
+}
 
-playAudio = function(){
-  console.log('clicked', play.song)
+var playAudio = function(){
   var a = new Audio(play.song);
   a.play();
 }
+
