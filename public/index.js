6bf972103d470c239d3b62865bbc8fab751391f7
Russell Williams <russellwilliamscc@gmail.com>
2016-07-23 21:10:32 +0100

    trying to add next button

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
-var play = {
-  song: null
-}
+// var play = {
+//   audio: null
+// }
+
+var audio = new Audio()
 
 var state = {
   singles: null
@@ -20,7 +22,14 @@ var main = function() {
   var btn = document.getElementById('playMusic');
   btn.onclick = playAudio;
 
-  // var select = document.getElementById('songList');
+  var btn = document.getElementById('pauseMusic');
+  btn.onclick = pauseAudio;
+
+  var btn = document.getElementById('nextSong');
+  btn.onclick = nextButton;
+
+  // drawing()
+  // init() 
 
 };
 
@@ -45,14 +54,12 @@ var searchSpotify = function(userInput) {
       var jsonString = request.responseText;
       spotifySearch = JSON.parse( jsonString );
       searchAlbums(spotifySearch)
-      // console.log('spotifySearch', spotifySearch)
     };
   };
   request.send( null );
 };
 
 var searchAlbums = function(spotifySearch){
-  // console.log("searchAlbums func", spotifySearch)
   var artist = spotifySearch.artists.items[0].href;
   var url = artist + "/albums?album_type=single";
   var request1 = new XMLHttpRequest();
@@ -60,67 +67,135 @@ var searchAlbums = function(spotifySearch){
   request1.onload = function(){
     if(request1.status === 200) {
       state.singles = JSON.parse(request1.responseText)
-      // console.log("sA, singles", singles)
-      dropDownList()
-      // var singles = singles.items[1].href
-      // console.log("that time?", x)
-      // searchForSong(singles)
+      // dropDownList()
+      displaySong()
     }
   };
   request1.send(null)
 };
 
+var displaySong = function(x){
+  i = 0
+  // i += x
+  var p = document.getElementById('song')
+  var opt = document.createElement('option')
+  opt.innerHTML = state.singles.items[i].name
+  p.appendChild(opt)
+  var song = state.singles.items[i].href
+  // console.log("what's this", song)
+  findSong(song)
+  // selectSong(i)
+  document.getElementById("pic").innerHTML = '<img class="background" src=' + state.singles.items[i].images[1].url + '>';
+}
+
+var nextButton = function(){
+  audio.pause()
+  var x 
+  x += 1
+  var song = state.singles.items[x].href
+  findSong(song)
+}
+
 var dropDownList = function(){
   var select = document.getElementById('songList');
-  // console.log("ddlist singles", singles)
   for ( var i = 0; i < state.singles.items.length; i++ ){
     var opt = document.createElement('option')
-    opt.innerHTML = i;
+    console.log("find name", state.singles.items[i].images[1].url)
+    opt.innerHTML = state.singles.items[i].name;
     opt.value = i;
-    // console.log("opt Value", opt)
     select.appendChild(opt)
+    document.getElementById("pic").innerHTML = '<img class="background" src=' + state.singles.items[i].images[1].url + '>';
+    // imageToShow = state.singles.items[i].images[1].url;
   }
 }
 
 var findSong = function(songSearch){
-  // console.log("search for a star", songSearch)
   var url = songSearch;
   var request1 = new XMLHttpRequest();
   request1.open("GET", url);
   request1.onload = function(){
     if(request1.status === 200) {
       var singles = JSON.parse(request1.responseText)
-      console.log("whaaaaat", singles.tracks.items[0].preview_url)
-      play.song = singles.tracks.items[0].preview_url
-      // var a = new Audio(play.song);
-      // a.play();
-      // console.log('play?', play.song)
+      // console.log("whaaaaat", singles.tracks.items[0].preview_url)
+      var song = singles.tracks.items[0].preview_url
+       audio = new Audio(song);
     }
   };
   request1.send(null)
 };
 
-  // var single = singles.items[i].href
 
-
-// var displayOptions = function(select) {
-//   for ( var i = 0; i < state.countries.length; i++ ) {
-//     var opt = document.createElement('option')
-//     opt.innerHTML = state.countries[i].name;
-//     opt.value = i;
-//     select.appendChild(opt)
-//   }
+// var selectSong = function(){
+//   // var i = 0
+//   // i = i
+//   // i =+ x
+//   // var index = document.getElementById('songList').value;
+//   var song = state.singles.items[i].href
+//   console.log("what's this", song)
+//   findSong(song)
 // }
 
-var selectSong = function(){
-  var index = document.getElementById('songList').value;
-  var song = state.singles.items[index].href
-  console.log("what's this", song)
-  findSong(song)
-}
+// var a = new Audio(play.song);
 
 var playAudio = function(){
-  var a = new Audio(play.song);
-  a.play();
+  console.log(audio)
+  audio.play();
 }
+
+var pauseAudio = function(){
+  audio.pause();
+}
+
+ //  // ---------------------------------
+
+//   var drawing = function(imageToShow){
+
+//   var canvas = document.getElementById( 'main' );
+//   console.log( 'canvas', canvas );
+//   var context = canvas.getContext( '2d' );
+//   context.fillStyle = 'grey'
+//   context.fillRect( 10, 10, 580, 380 );
+//   var img = document.getElementById("imageToShow");
+//   ctx.drawImage(img, 10, 10, 150, 180);
+
+// }
+
+// var can, ctx, step, steps = 0,
+     // delay = 10;
+
+// function init() {
+//    can = document.getElementById("main");
+//    ctx = can.getContext("2d");
+//    ctx.font = "20pt Verdana";
+//    ctx.textAlign = "center";
+//    ctx.textBaseline = "middle";
+//    step = 320;
+//    steps = 50;
+//    RunTextLeftToRight();
+// }
+
+// function RunTextLeftToRight() {
+//    step --;
+//    ctx.clearRect(0, 0, can.width, can.height);
+//    ctx.save();
+//    ctx.translate(step, can.height / 2);
+//    ctx.fillText("Welcome", 0, 0);
+//    ctx.restore();
+//    if (step == steps)
+//        step = 320;
+//    if (step > steps)
+//        var t = setTimeout('RunTextLeftToRight()', delay);
+// }
+     
+
+ // context.beginPath();
+ // context.moveTo(100, 100);
+ // context.lineTo(100, 150);
+ // context.stroke();
+
+
+
+  
+
+ //  // ---------------------------------
 
