cf9d75c523a2d1e616af357675bf1e88d4e08604
Russell Williams <russellwilliamscc@gmail.com>
2017-01-21 18:44:46 +0000

    major CSS changes

--- app/index.js
+++ app/index.js
@@ -2,6 +2,12 @@ window.onload = function(){
   main()
 };
 
+var state = {
+  albums: null,
+  tracks: null,
+  artist: null
+}
+
 var main = function() {
 
   var btn = document.getElementById('searchSpotify');
@@ -16,7 +22,7 @@ var main = function() {
   var btn = document.getElementById('pauseMusic');
   btn.onclick = pauseAudio;
 
-  var select = document.getElementById('songList');
+  var select = document.getElementById('album');
   select.onchange = selectAlbum;
 
   var select = document.getElementById('trackList');
@@ -44,9 +50,9 @@ var searchArtists = function(userInput) {
     if (request.status === 200){
       var jsonString = request.responseText;
       artistSearch = JSON.parse( jsonString );
-      choice.artist = artistSearch.artists.items[0];
+      state.artist = artistSearch.artists.items[0];
       searchAlbums(artistSearch)
-      relatedArtists(artistSearch)
+      // relatedArtists(artistSearch)
     };
   };
   request.send( null );
@@ -62,7 +68,7 @@ var searchAlbums = function(artistSearch){
   request1.onload = function(){
     if(request1.status === 200) {
       state.albums = JSON.parse(request1.responseText)
-      findArtist()
+      generateAlbums()
     }
   };
   request1.send(null)
@@ -89,34 +95,53 @@ var setRelatedArtists = function(related){
     document.getElementById("similarArtists").innerHTML  += related.artists[i].name + "<br>" 
 }
 
-var findArtist = function(){
-  document.getElementById("songList").options.length = 0;
-  var select = document.getElementById('songList');
+var generateAlbums = function(){
+  document.getElementById("album").options.length = 0;
+  var select = document.getElementById('album');
+  var placeholder = document.createElement('option')
+  text = "choose an album"
+  placeholder.innerHTML = text
+  select.appendChild(placeholder)
+
   for ( var i = 0; i < state.albums.items.length; i++ ){
     var opt = document.createElement('option')
-    opt.innerHTML = state.albums.items[i].name;
+    album = state.albums.items[i].name;
+    album = album.substring(0,18);
+    console.log("opt:", album)
+    opt.className="opt";
+    opt.innerHTML = album;
     opt.value = i;
-    select.appendChild(opt)
-
+    select.appendChild(opt);
     setArtistPic()
+  // }
 
+
+  // for ( var i = 0; i < state.albums.items.length; i++ ){
+  //   var opt = document.createElement('option')
+  //   opt.innerHTML = state.albums.items[i].name;
+  //   console.log("opt:", opt)
+  //   // opt = opt.substring(0,18);
+  //   opt.className="opt";
+  //   opt.value = i;
+  //   select.appendChild(opt);
+  //   setArtistPic()
   }
 }
 
 var setArtistPic = function(){
   document.getElementById("pic").innerHTML = "<h3>"
-  + choice.artist.name + "</h3>" +'<img class="picture" src=' + choice.artist.images[1].url + '>';
+  + state.artist.name + "</h3>" +'<img class="picture" src=' + state.artist.images[1].url + '>';
 }
 
 var selectAlbum = function(){
-  var index = document.getElementById('songList').value;
+  var index = document.getElementById('album').value;
   var album = state.albums.items[index].href
   setAlbumPic(index, album)
   getSongs(album)
 }
 
 var setAlbumPic = function(index, album){
-  document.getElementById("albumPic").innerHTML = '<img class="picture" src=' + state.albums.items[index].images[1].url + '>';
+  document.getElementById("albumPic").innerHTML = '<div class="artist-pic">' + '<img class="picture" src=' + state.albums.items[index].images[1].url + '>' + '</div>';
 }
 
 var getSongs = function(albums){
@@ -126,7 +151,7 @@ var getSongs = function(albums){
   request1.onload = function(){
     if(request1.status === 200) {
       var albumInfo = JSON.parse(request1.responseText)
-      album.tracks = albumInfo.tracks.items;
+      state.tracks = albumInfo.tracks.items;
       generateTrackList(albumInfo);
     }
   };
@@ -138,26 +163,38 @@ var generateTrackList = function(albumInfo){
   + albumInfo.name + "</h3>"
   document.getElementById("trackList").options.length = 0;
   var select = document.getElementById('trackList');
-  for ( var i = 0; i < album.tracks.length; i++ ){
+  var placeholder = document.createElement('option')
+  text = "choose a song"
+  placeholder.innerHTML = text
+  select.appendChild(placeholder)
+  for ( var i = 0; i < state.tracks.length; i++ ){
     var opt = document.createElement('option')
-    song = album.tracks[i].name;
+    song = state.tracks[i].name;
+    song = song.substring(0,18);
+    console.log("this thing : ", song)
+    opt.className="opt"
     opt.innerHTML = song;
     opt.value = i;
+    // select.appendChild("choose")
     select.appendChild(opt)
   }
 }
 
+
+
+
+
 var createTrack = function() {
   var index = document.getElementById('trackList').value;
-  var song = album.tracks[index].preview_url
+  var song = state.tracks[index].preview_url
   audio = new Audio(song)
 
-  var str = album.tracks[index].name
-  var string = str.substring(0,25) +"...";
+  var str = state.tracks[index].name
+  var string = str.substring(0,30);
   console.log("String-a-ding", string)
 
-  document.getElementById("artist").innerText = "artist: " + choice.artist.name
-  document.getElementById("title").innerText = "track: " + string
+  document.getElementById("artist").innerText = "artist : " + state.artist.name
+  document.getElementById("title").innerText = "track : " + string
 }
 
 var audio = new Audio()
@@ -166,30 +203,32 @@ var playAudio = function(){
   audio.play();
   console.log("YEAH!")
   var spool = document.getElementById("leftSpoolCircle")
+  document.getElementById("leftSpoolCircle").style.transition = "all 30s, linear";
   spool.style.webkitTransform = "rotateZ(-900deg)";
   var spool = document.getElementById("rightSpoolCircle")
+  document.getElementById("rightSpoolCircle").style.transition = "all 30s, linear";
   spool.style.webkitTransform = "rotateZ(-900deg)";
 }
 
 var pauseAudio = function(){
   audio.pause();
-  style.webkitAnimationPlayState = 'paused';
-  var spool = document.getElementById("leftSpoolCircle");
+  var spool = document.getElementById("leftSpoolCircle")
   spool.style.webkitAnimationPlayState = 'paused';
-  // document.getElementById("artist").innerText = ""
-  // document.getElementById("title").innerText = ""
-
+  var spool = document.getElementById("rightSpoolCircle")
+  spool.style.webkitAnimationPlayState = 'paused';
 }
 
-var choice = {
-  artist: null
-}
+// var choice = {
+//   artist: null
+// }
 
-var album = {
-  tracks: null
-}
+// var album = {
+//   tracks: null
+// }
 
-var state = {
-  albums: null,
-}
+// var state = {
+//   albums: null,
+//   tracks: null,
+//   artist: null;
+// }
 
